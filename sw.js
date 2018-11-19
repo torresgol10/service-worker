e5656a2aafee32dd7b184f37e47ade08b5fffa08
torresgol10 <40922354+torresgol10@users.noreply.github.com>
2018-11-19 21:43:54 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,46 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.3.1/workbox-sw.js');
 workbox.routing.registerRoute(
   new RegExp('.*\.js'),
-  workbox.strategies.networkFirst()
+  workbox.strategies.networkFirst({
+    cacheName: 'cache-js',
+    plugins: [
+      new workbox.expiration.Plugin({
+        // Keep at most 50 entries.
+        maxEntries: 50,
+        // Don't keep any entries for more than 30 days.
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+        // Automatically cleanup if quota is exceeded.
+        purgeOnQuotaError: true,
+      }),
+    ],
+  })
+  
 );
 
 workbox.routing.registerRoute(
   // Cache CSS files
   /.*\.css/,
   // Use cache but update in the background ASAP
-   workbox.strategies.networkFirst()
-);
-
-workbox.routing.registerRoute(
-  // Cache image files
-  /.*\.(?:png|jpg|jpeg|svg|gif)/,
-  // Use the cache if it's available
-  workbox.strategies.cacheFirst({
-    // Use a custom cache name
-    cacheName: 'image-cache',
+   workbox.strategies.networkFirst({
+    cacheName: 'cache-css',
     plugins: [
       new workbox.expiration.Plugin({
-        // Cache only 20 images
-        maxEntries: 20,
-        // Cache for a maximum of a week
-        maxAgeSeconds: 7 * 24 * 60 * 60,
-      })
+        // Keep at most 50 entries.
+        maxEntries: 50,
+        // Don't keep any entries for more than 30 days.
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+        // Automatically cleanup if quota is exceeded.
+        purgeOnQuotaError: true,
+      }),
     ],
   })
 );
+
 self.addEventListener('install', function(e) {
  e.waitUntil(
-   caches.open('airhorner').then(function(cache) {
+   caches.open('offline').then(function(cache) {
      return cache.addAll([
-       '/index.html',
+       '/',
      ]);
    })
  );
